ac2862e012083092a006095057604221534f0188
Gabriel Alves <37423798+GabrielUPF@users.noreply.github.com>
2018-04-26 19:44:28 -0300

    Update cadastro.php

--- cadastro/cadastro.php
+++ cadastro/cadastro.php
@@ -7,7 +7,17 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.0/jquery.mask.js"></script>
+    <script>
+        $(document).ready(function () { 
+            var $cpf = $("#cpf");
+            $cpf.mask('000.000.000-00');
+            
+            var $telefone = $("#telefone");
+            $telefone.mask('(00) 0 0000-0000');
+        });
+    </script>
+    
     <nav aria-label="breadcrumb">
         <ol class="breadcrumb">
             <li class="breadcrumb-item active" aria-current="page"><h4>Cadastro de Clientes</h4></li>
@@ -23,24 +33,24 @@
         <h3>Dados Pessoais</h3>
         
         <div class="form-group">
-            <label for="nome">Nome</label>
+            <label for="nome">Nome*</label>
             <input type="text" style="width:400px" class="form-control" id="nome" name="nome" required>
         </div>
         
         <div class="form-group">
-            <label for="cpf">CPF</label>
+            <label for="cpf">CPF*</label>
             <input type="text" style="width:300px" class="form-control" maxlength="14" id="cpf" placeholder="Ex.: 000.000.000-00" name="cpf" required>
         </div>
         </br>
         <h3>Contato</h3>
         
         <div class="form-group">
-            <label for="email">E-mail</label>
+            <label for="email">E-mail*</label>
             <input type="text" style="width:400px" class="form-control" id="email" placeholder="Ex.: contato@exemplo.com" name="email" required>
         </div>
         
         <div class="form-group">
-            <label for="telefone">Telefone</label>
+            <label for="telefone">Telefone*</label>
             <input type="text" style="width:300px" class="form-control" maxlength="16" id="telefone" placeholder="Ex.: (00) 0 0000-0000" name="telefone" required>
         </div>
         </br>
@@ -49,30 +59,31 @@
         <h3>Endereço</h3>
         
         <div class="form-group">
-            <label for="cidade">Cidade</label>
+            <label for="cidade">Cidade*</label>
             <input type="text" style="width:350px" class="form-control" id="cidade" name="cidade" required>
         </div>
         
         <div class="form-group">
-            <label for="rua">Rua</label>
+            <label for="rua">Rua*</label>
             <input type="text" style="width:350px" class="form-control" id="rua" name="rua" required>
         </div>
 
         <div class="form-group">
-            <label for="numero">Número</label>
+            <label for="numero">Número*</label>
             <input type="number" style="width:100px" class="form-control" id="numero" name="numero" required>
         </div>
 
         <div class="form-group">
             <label for="bairro">Bairro</label>
-            <input type="text" style="width:250px" class="form-control" id="bairro" name="bairro" required>
+            <input type="text" style="width:250px" class="form-control" id="bairro" name="bairro" >
         </div>
         
         <div class="form-group">
             <label for="complemento">Complemento</label>
-            <input type="text" style="width:250px" class="form-control" id="complemento" name="complemento" required>
-        </div>
+            <input type="text" style="width:250px" class="form-control" id="complemento" name="complemento">
+        </div> 
     </div>
+    <h3><p> (*) Campos Obrigatórios </p></h3>
     </div>
     <button type="submit" class="btn btn-primary">Cadastrar</button>
     </form>
